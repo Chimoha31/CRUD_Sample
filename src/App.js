a7d8d7b1a45afe0b62662b4132ff749683074737
Chimoha31 <mokochii1108@gmail.com>
2022-05-12 00:15:22 -0700

    Added components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
-import React from 'react';
+import React, { Fragment } from "react";
+import AddBook from "./components/AddBook";
+import { Col, Container, Navbar, Row } from "react-bootstrap";
+import BooksList from "./components/BooksList";
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <Fragment>
+      <Navbar bg="light" variant="dark" className="header">
+        <Container>
+          <Navbar.Brand href="#home">Library - Firebase CRUD -</Navbar.Brand>
+        </Container>
+      </Navbar>
 
-export default App
+      <Container style={{width: "400px"}}>
+        <Row>
+          <Col>
+            <AddBook />
+          </Col>
+        </Row>
+      </Container>
 
+      <Container>
+        <Row>
+          <Col>
+            <BooksList />
+          </Col>
+        </Row>
+      </Container>
+    </Fragment>
+  );
+};
+
+export default App;
